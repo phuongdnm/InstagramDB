dd2450e929ffcb00d3fb7392f901c0881ea884f5
Phuong Minh <phuongminh2303@gmail.com>
2019-05-18 22:25:36 +0700

    Create tags schema with 2 tables

--- instagram.sql
+++ instagram.sql
@@ -44,6 +44,20 @@ CREATE TABLE follows(
     PRIMARY KEY (follower_id, followee_id)
 );
 
+CREATE TABLE tags(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    tag_name VARCHAR(255) UNIQUE, 
+    create_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE photo_tags(
+    photo_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY(photo_id) REFERENCES photos(id),
+    FOREIGN KEY(tag_id) REFERENCES tags(id),
+    PRIMARY KEY(photo_id, tag_id)
+);
+
 INSERT INTO users(username) VALUES
 ('Captain'),
 ('IronMan'),
