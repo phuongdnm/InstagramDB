6686c33b3e5e6566ab4ab453a418623c0da910a8
Phuong Minh <phuongminh2303@gmail.com>
2019-05-18 18:24:08 +0700

    Create table 'comments' and data

--- instagram.sql
+++ instagram.sql
@@ -16,6 +16,16 @@ CREATE TABLE photos(
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 
+CREATE TABLE comments(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    comment_text VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    photo_id INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (photo_id) REFERENCES photos(id)
+);
+
 INSERT INTO users(username) VALUES
 ('Captain'),
 ('IronMan'),
@@ -25,6 +35,11 @@ INSERT INTO photos(image_url, user_id) VALUES
 ('/image1', 1),
 ('/image2', 2),
 ('/image3', 2);
+
+INSERT INTO comments(comment_text, user_id, photo_id) VALUES
+('I can do this all day', 1, 2),
+('I am Iron man', 3 , 2),
+('I know it', 2, 1);
 
 -- SELECT 
 --     photos.image_url,
