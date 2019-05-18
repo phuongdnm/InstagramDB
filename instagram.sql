455c039d8cebac8b7269fb214d8a3c529bc6ed35
Phuong Minh <phuongminh2303@gmail.com>
2019-05-18 18:18:03 +0700

    Crete table 'photos' and data

--- instagram.sql
+++ instagram.sql
@@ -8,8 +8,28 @@ CREATE TABLE users(
     created_at TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE photos(
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    image_url VARCHAR(255) NOT NULL,
+    user_id INTEGER NOT NULL,
+    create_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
 INSERT INTO users(username) VALUES
 ('Captain'),
 ('IronMan'),
 ('Thor');
+
+INSERT INTO photos(image_url, user_id) VALUES
+('/image1', 1),
+('/image2', 2),
+('/image3', 2);
+
+-- SELECT 
+--     photos.image_url,
+--     users.username
+-- FROM photos
+-- JOIN users
+--     ON photos.user_id = users.id;
 
